cf1578f7e903bbe51924be81731383222c37785d
zhou.jinyu <zhou.jinyu@navercorp.com>
2017-01-15 11:55:40 +0800

    aa

--- myconfig.sh
+++ myconfig.sh
@@ -1,3 +1,3 @@
-./configure --prefix=/Users/YoScript/Repo/file/nginx/build --with-pcre=/Users/YoScript/Repo/file/nginx/sdk/pcre-8.39 --with-debug
+./configure --prefix=~/Repo/file/nginx/build --with-pcre=~/Repo/file/nginx/sdk/pcre-8.39 --with-debug
 sudo make && sudo make install
 
